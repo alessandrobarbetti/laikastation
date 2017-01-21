46a24a69410ca13d87c392d0b7617e1d61ae2cf2
alessandro_barbetti <alessandro_barbetti@hotmail.it>
2017-01-21 22:35:02 +0100

    added graph
    added graph

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -51,11 +51,40 @@ $$(document).on('deviceready', function() {
     ble.startScan([], function(device) { devices.push(device); }, function(){});
     
     setTimeout(ble.stopScan, millisScan,
-        function() { listDevices(devices); },
+        function() { listDevices(devices);  createGraph(); },
         function() { console.log("stopScan failed"); }
     );
 
 });
+
+function createGraph(){
+    var canvas = document.getElementById('updating-chart'),
+    ctx = canvas.getContext('2d'),
+    startingData = {
+      labels: [1, 2, 3, 4, 5, 6, 7],
+      datasets: [
+          {
+              fillColor: "rgba(220,220,220,0.2)",
+              strokeColor: "rgba(220,220,220,1)",
+              pointColor: "rgba(220,220,220,1)",
+              pointStrokeColor: "#fff",
+              data: [65, 59, 80, 81, 56, 55, 40]
+          }
+      ]
+    },
+    latestLabel = startingData.labels[6];
+
+    // Reduce the animation steps for demo clarity.
+    var myLiveChart = new Chart(ctx).Line(startingData, {animationSteps: 15});
+
+
+    setInterval(function(){
+      // Add two random numbers for each dataset
+      myLiveChart.addData([Math.random() * 100, Math.random() * 100], ++latestLabel);
+      // Remove the first point so we dont just add values forever
+      myLiveChart.removeData();
+    }, 500);
+}
 
 function listDevices(devices){
     
